204c43909efa9a2dd49a3fb4c27f4abb704d378d
jaydeluca <jaydeluca4@gmail.com>
2021-08-14 12:44:30 -0400

    update python starter script

--- scripts/python-args-starter.py
+++ scripts/python-args-starter.py
@@ -4,22 +4,33 @@
 # Example usage: python3 python-args-starter.py --input "test input" --file test.txt
 
 import argparse
+import os.path
+from os import path
+import logging
+
+class Main(object):
+    def __init__(self, args):
+        if args.file:
+            print(f"File input: {args.file}")
+
+            # Parse comma separated csv
+            if path.exists(args.file):
+                input_file = open(args.file, 'r')
+                for line in input_file:
+                    (item1, item2) = line.strip().split(",")
+
+            else:
+                logging.error(f"Unable to find file {args.file}")
 
 
-parser = argparse.ArgumentParser(description='Script')
-parser.add_argument("-f", "--file",
-                    help="file input")
-parser.add_argument("-i", "--input",
-                    help="input data")
-args = parser.parse_args()
-
-
-def main():
-    if args.file:
-        print(f"File input: {args.file}")
-
-    if args.input:
-        print(f"Input: {args.input}")
+        if args.input:
+            print(f"Input: {args.input}")
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Script')
+    parser.add_argument("-f", "--file",
+                        help="file input")
+    parser.add_argument("-i", "--input",
+                        help="input data")
+    arguments = parser.parse_args()
+    Main(arguments)
